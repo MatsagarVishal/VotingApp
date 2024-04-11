a7c580d04c8f847b56e146a80018cf3ebf6c926d
MatsagarVishal <vishalmatsagar0007@gmail.com>
2024-04-12 02:36:10 +0530

    morgan added

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-// const dbURL =;
+const URI =process.env.url;
 
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/votingApp', {
+mongoose.connect( URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex:true
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express= require("express");
 const app=express();
-
+const morgan = require("morgan");
 const bodyparser= require("body-parser");
 app.use(bodyparser.json());
 // Databse connection
@@ -14,7 +14,8 @@ const userRoute=require("./routes/userRoute");
 require('dotenv');
 const PORT = process.env.PORT || 3300;
 
-
+// seting up morgon
+app.use(morgan("tiny"));
 app.use("/",userRoute);
 
 app.listen(PORT,()=>{
